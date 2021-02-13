f673b167baaf2e30bf860b3c7b4a70c51a111e3b
Saiful Islam <saifulsaif321@gmail.com>
2021-02-13 21:45:28 +0600

    model class added

---
+++ app/src/main/java/com/saiful/unsplashimagesearchapp/UnsplashPhoto.kt
@@ -0,0 +1,28 @@
+package com.saiful.unsplashimagesearchapp
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class UnsplashPhoto(
+        val id: String,
+        val description: String?,
+        val urls: UnsplashPhotoUrls,
+        val user: UnsplashUser
+) : Parcelable {
+
+    @Parcelize
+    data class UnsplashPhotoUrls(
+            val raw: String,
+            val full: String,
+            val regular: String,
+            val small: String,
+            val thumb: String,
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+            val name: String,
+            val username: String,
+    ) : Parcelable
+}
