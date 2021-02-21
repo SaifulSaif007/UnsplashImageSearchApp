29d5cb172d50025c990d5a02b7349dc66224ac69
Saiful Islam <saifulsaif321@gmail.com>
2021-02-21 14:31:18 +0600

    Navigation graph & fragment added

---
+++ app/src/main/java/com/saiful/unsplashimagesearchapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package com.saiful.unsplashimagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.saiful.unsplashimagesearchapp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery){
+
+}
