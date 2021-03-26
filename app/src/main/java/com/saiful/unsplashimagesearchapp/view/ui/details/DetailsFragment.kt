a1562d01fbd911b5b954cb5ca0523a9561e5425f
Saiful Islam <saifulsaif321@gmail.com>
2021-03-27 01:08:38 +0600

    - DetailsFragment UI done

---
+++ app/src/main/java/com/saiful/unsplashimagesearchapp/view/ui/details/DetailsFragment.kt
@@ -0,0 +1,9 @@
+package com.saiful.unsplashimagesearchapp.view.ui.details
+
+import androidx.fragment.app.Fragment
+import com.saiful.unsplashimagesearchapp.R
+
+class DetailsFragment  : Fragment(R.layout.fragment_details) {
+
+
+}
