b806d17224664efd3b6c3e58e3cb96384082eaa0
Saiful Islam <saifulsaif321@gmail.com>
2023-06-03 00:43:19 +0600

    update gradle version

--- buildSrc/src/main/java/ConfigData.kt
+++
@@ -1,9 +0,0 @@
-object ConfigData {
-    const val appId = "com.saiful.unsplashimagesearchapp"
-    const val compileSdkVersion = 33
-    const val minSdkVersion = 24
-    const val targetSdk = 33
-    const val versionCode = 1
-    const val versionName = "1.0.0"
-
-}
