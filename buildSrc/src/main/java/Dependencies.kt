228f5e19feab2816757237b97ef2d7e975ae039b
Saiful Islam <saifulsaif321@gmail.com>
2023-06-03 00:49:44 +0600

    update gradle version

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,32 @@
+object BuildPlugins {
+    val android by lazy { "com.android.tools.build:gradle:${Versions.gradlePlugin}" }
+    val kotlin by lazy { "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}" }
+    val safeArgs by lazy { "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.safeArgs}" }
+    val hiltPlugin by lazy { "com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}" }
+
+}
+
+object Dependencies {
+    val kotlinStdLib by lazy { "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}" }
+    val ktx by lazy { "androidx.core:core-ktx:${Versions.kotlin}" }
+    val appCompat by lazy { "androidx.appcompat:appcompat:${Versions.appCompat}" }
+    val material by lazy { "com.google.android.material:material:${Versions.appCompat}" }
+    val constraintLayout by lazy { "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}" }
+
+    val retrofit by lazy { "com.squareup.retrofit2:retrofit:${Versions.retrofit}" }
+    val gson by lazy { "com.squareup.retrofit2:converter-gson:${Versions.retrofit}" }
+    val hilt by lazy { "com.google.dagger:hilt-android:${Versions.hilt}" }
+    val hiltAnnotation by lazy { "com.google.dagger:hilt-compiler:${Versions.hilt}" }
+    val navigationFragment by lazy { "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}" }
+    val navigationUi by lazy { "androidx.navigation:navigation-ui-ktx:${Versions.navigation}" }
+    val glide by lazy { "com.github.bumptech.glide:glide:${Versions.glide}" }
+    val paging by lazy { "androidx.paging:paging-runtime-ktx:${Versions.paging}" }
+    val dataStore by lazy { "androidx.datastore:datastore-preferences:${Versions.dataStore}" }
+
+    val leakCanary by lazy { "com.squareup.leakcanary:leakcanary-android:${Versions.leakCanary}" }
+
+    val testJunit by lazy { "junit:junit:${Versions.testJunit}" }
+    val androidTestJunit by lazy { "androidx.test.ext:junit:${Versions.androidTestJunit}" }
+    val espressoCore by lazy { "androidx.test.espresso:espresso-core:${Versions.espressoCore}" }
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,25 @@
+object Versions {
+
+    // Plugins
+    const val gradlePlugin = "8.0.0"
+    const val kotlin = "1.7.0"
+    const val safeArgs = "2.5.3"
+
+    // Libs
+    const val retrofit = "2.9.0"
+    const val hilt = "2.42"
+    const val hiltViewModel = "1.0.0-alpha03"
+    const val navigation = "2.4.2"
+    const val glide = "4.11.0"
+    const val paging = "3.0.0-alpha05"
+    const val dataStore = "1.0.0-beta02"
+
+    const val testJunit = "4.+"
+    const val androidTestJunit = "1.1.2"
+    const val espressoCore = "3.3.0"
+
+    const val appCompat = "1.3.0"
+    const val constraintLayout = "2.0.4"
+
+    const val leakCanary = "2.9.1"
+}
